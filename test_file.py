6f21e6bd55f3f2672a0f3c1e470708a9e006f21e
Tatoshechka322 <145149241+Tatoshechka322@users.noreply.github.com>
2024-11-22 15:07:51 +0700

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,10 +1,10 @@
-from discriminant import calculate_discriminant  
+from discriminant import calculate_discriminant
 
 
 def test_positive_discriminant():
-    assert calculate_discriminant(1, 2, -3) == 16
-    assert calculate_discriminant(1, 4, 4) == 0
+  assert calculate_discriminant(1, 2, -3) == 16
+  assert calculate_discriminant(1, 4, 4) == 0
 
 
-def test_ngative_discriminant():
-    assert calculate_discriminant(1, 2, 3) == -8
+def test_negative_discriminant():
+  assert calculate_discriminant(1, 2, 3) == -8
